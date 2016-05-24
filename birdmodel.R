3f8fd5f563b544301026bde153608424cc3e6a54
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2016-05-23 20:21:47 -0700

    Checkpoint. Not happy with points-on-mean lines.

--- birdmodel.R
+++ birdmodel.R
@@ -1,26 +1,49 @@
 # Bird waste, in grams, per day
-# The mean bird weight is 400 grams; the baseline random
-# variable for waste averages one tenth of the bird weight.
-# A filter is used to model the effect of the bird crop; little
-# waste on day n makes more waste likely on day n+1.
-birdwaste <- function(n) {
+# The mean bird weight is 400 grams; the baseline mean 
+# value for waste is one tenth of the bird weight.
+# Two normal distributions are created: a low-mean distribution for use
+# when the previous day had high waste, and a high-mean distribution for
+# use when the previous day had low waste.
+
+birdweight <- function() {
   weight <- rnorm(1, 400, 40)
-  waste <- rep(0, n)
-  mean <- weight * 0.1
+  return(weight) 
+}
+
+next_weight <- function(cur_weight, baseline_mean, low_mean, high_mean) {
+  nw <- ifelse(
+    cur_weight > baseline_mean,
+    rnorm(1, low_mean, baseline_mean * .3),
+    rnorm(1, high_mean, baseline_mean * .3)
+  )
+  # Negative waste makes no sense - just limit to 0
+  if (nw < 0) nw <- 0;
+  return(nw)
+}
+
+birdwaste <- function(num_days) {
+  weight <- birdweight()
+  baseline_mean <- weight * 0.1
+  waste <- rep(0, num_days)
+
   # "bimodality": if 1, you'll get a bell curve.
-  # lower values spread the two peaks apart.
-  bimodality <- .8
-  mean0 <- mean * bimodality
-  mean1 <- mean / bimodality
-  waste[1] <- rnorm(1, mean, mean * .1)
-  for (i in 2:n) {
-    waste[i] <- ifelse(
-      waste[i - 1] > mean,
-      rnorm(1, mean0, mean * .1),
-      rnorm(1, mean1, mean * .1)
-    )
+  # As the value goes below one, two peaks emerge, centered around the baseline mean.
+  bimodality <- 0.6
+  low_mean <- baseline_mean * bimodality
+  high_mean <- baseline_mean / bimodality
+  waste[1] <- rnorm(1, baseline_mean, baseline_mean * .1)
+  for (i in 2:num_days) {
+    waste[i] <- next_weight(waste[i - 1], baseline_mean, low_mean, high_mean)
   }
-  # Negative waste makes no sense - just reflect around y=0
-  waste <- abs(waste)
-  return(data.frame(day=1:n, waste=waste))
+  return(data.frame(day=1:num_days, waste=waste))
+}
+
+multi_means <- function(num_days, num_birds) {
+  means <- numeric(0)
+  for (i in 1:num_birds) {
+    w <- birdwaste(num_days)
+    means <- append(means, mean(w$waste))
+
+  }
+  return(means)
 }
--- plot.R
+++ plot.R
@@ -28,6 +28,32 @@ bw_line <- function(w) {
   return(p)
 }
 
-multi_hist <- function(n) {
+multi_hist <- function(num_days, num_plots, alphahist = 0.05, alphamean = 0.05, meanpoints = FALSE) {
+  means <- numeric(0)
+  p <- ggplot() + xlab("Waste (g)")
+  for (i in 1:num_plots) {
+    w <- birdwaste(num_days)
+    means <- append(means, mean(w$waste))
+    p <- p + geom_histogram(data=w, aes(waste), binwidth=1, alpha = alphahist)
+  }
   
+  # plot the mean lines last.
+  for (m in means) {
+    p <- p + 
+      geom_vline(xintercept = m, color = "red", size = 1, alpha = alphamean)
+  }
+  
+  if (meanpoints) {
+    meandf <- data.frame(x=means, y=4)
+    p <- p + 
+      geom_point(data=meandf, aes(x, y), alpha = alphamean * 2, color = "red", position = "jitter")
+  }
+  
+  return(list(plot = p, means = means))    
+}
+
+clt_hist <- function(num_days, num_birds) {
+  means <- data.frame(waste = multi_means(num_days, num_birds))
+  p <- ggplot() + xlab("Waste (g)")
+  p <- p + geom_histogram(data=means, aes(waste), binwidth=0.5)
 }
